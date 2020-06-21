f7b8ecd93c033e5b29a7908eab8939af8f2f6e04
Austin Bingham <austin.bingham@gmail.com>
2020-06-21 17:11:57 +0200

    Bump version: 0.0.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cosmic-ray-spor-filter',
-    version="0.0.0",
+    version="1.0.0",
     packages=find_packages('src'),
 
     author='Austin Bingham',
